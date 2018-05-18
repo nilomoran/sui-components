3dba1c732d1a2ab31d5ee02cb4f5533d77737ae2
JuanMa Garrido <juanma.garrido@gmail.com>
2018-05-18 13:03:26 +0200

    feat(atom/imagePlaceholder): component creation

---
+++ components/atom/imagePlaceholder/README.md
@@ -0,0 +1,23 @@
+# AtomImagePlaceholder
+
+> Description
+
+<!-- ![](./assets/preview.png) -->
+
+## Installation
+
+```sh
+$ npm install @s-ui/react-atom-imagePlaceholder --save
+```
+
+## Usage
+
+### Basic usage
+```js
+import AtomImagePlaceholder from '@s-ui/react-atom-imagePlaceholder'
+
+return (<AtomImagePlaceholder />)
+```
+
+
+> **Find full description and more examples in the [demo page](#).**
---
+++ components/atom/imagePlaceholder/src/index.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+// import PropTypes from 'prop-types'
+
+class AtomImagePlaceholder extends Component {
+  render () {
+    return (
+      <div className='react-AtomImagePlaceholder'>
+        <h1>AtomImagePlaceholder</h1>
+      </div>
+    )
+  }
+}
+
+AtomImagePlaceholder.displayName = 'AtomImagePlaceholder'
+
+// Remove these comments if you need
+// AtomImagePlaceholder.contextTypes = {i18n: PropTypes.object}
+// AtomImagePlaceholder.propTypes = {}
+// AtomImagePlaceholder.defaultProps = {}
+
+export default AtomImagePlaceholder
